e3efbd56e6c400ddc03cf54e56ac4b4916ff0ed9
Piers Chambers <piers@varyonic.com>
2026-01-06 16:59:01 -0500

    Replace dartsass-sprockets with dartsass-rails for Propshaft compatibility
    - Replace dartsass-sprockets dependency with dartsass-rails
    - Add require for action_view (needed by arbo, previously loaded by sprockets)
    - Make assets precompile initializer conditional on Sprockets availability
    - Remove Sprockets-specific config from test template (manifest.js, assets.rb, assets.precompile)
    - Update stylesheets_spec to skip when Sprockets is not available
    
    This change enables ActiveAdmin to work with Rails 8's default Propshaft asset pipeline
    while maintaining backward compatibility with Sprockets in earlier Rails versions.

--- spec/requests/stylesheets_spec.rb
+++ spec/requests/stylesheets_spec.rb
@@ -2,16 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Stylesheets", type: :request do
 
-  require "sprockets"
+  begin
+    require "sprockets"
+    sprockets_available = true
+  rescue LoadError
+    sprockets_available = false
+  end
 
   let(:css) do
     assets = Rails.application.assets
     assets.find_asset("active_admin.css")
   end
-  it "should successfully render the scss stylesheets using sprockets" do
+  
+  it "should successfully render the scss stylesheets using sprockets", skip: !sprockets_available do
     expect(css).to_not eq nil
   end
-  it "should not have any syntax errors" do
+  
+  it "should not have any syntax errors", skip: !sprockets_available do
     expect(css.to_s).to_not include("Syntax error:")
   end
 
