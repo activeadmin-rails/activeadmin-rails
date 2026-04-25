6fc64550fbaf8fca8c96c7143d242e43fa8eb1bc
Piers Chambers <piers@varyonic.com>
2026-04-25 17:44:29 -0500

    Fix circular reference to print.scss

--- spec/support/templates/dartsass.rb
+++ spec/support/templates/dartsass.rb
@@ -1,4 +1,4 @@
 Rails.application.config.dartsass.builds = {
   "active_admin.scss" => "active_admin.css",
-  "active_admin/print.scss" => "active_admin/print.css"
+  "active_admin_print.scss" => "active_admin/print.css"
 }
